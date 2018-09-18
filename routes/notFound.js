e0812302b57b0d4fc492a246cf2d442705c24c1a
bad4iz <bad4iz@gmail.com>
2018-09-18 15:19:20 +0400

    обработка ошибок кнопочки на главную refactoring finish

--- routes/notFound.js
+++ routes/notFound.js
@@ -1,16 +1,5 @@
-const render = require('../lib/render');
-
 function notFound(req, res) {
-  render('error.html', {error: 'не найдено'}, (error, html) => {
-    if (error) {
-      res.writeHead(500, {'Content-Type': 'text/plain'});
-      return res.end(error.message);
-    }
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(html)
-  });
+  res.render('error.html', {error: 'не найдено'});
 }
 
 module.exports = notFound;
--- routes/search.js
+++ routes/search.js
@@ -1,46 +1,20 @@
-// const fs = require('fs');
-// const path = require('path');
 const url = require('url');
 
 const omdb = require('../lib/omdb');
-const render = require('../lib/render');
 
 function search(req, res) {
   const parseUrl = url.parse(req.url, true);
-  const { title } = parseUrl.query;
+  const {title} = parseUrl.query;
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-
-  omdb(title, (error, movie) =>{
+  omdb(title, (error, movie) => {
     if (error) {
-      render('error.html', { error: error.message}, (error, html) => {
-        if(error) {
-          res.writeHead(500, {'Content-Type' : 'text/plain'});
-          return res.end(error.message);
-
-        }
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(html)
-      });
+      res.render('error.html', {error: error.message});
     } else {
 
-      render('movie.html', movie, (error, html) => {
-        if (error) {
-          res.writeHead(500, {'Content-Type': 'text/plain'});
-          return res.end(error.message);
-        }
-
-        res.statusCode = 200;
-        res.end(html)
-      });
+      res.render('movie.html', movie);
     }
   });
 
-  // const stream = fs.createReadStream(path.join('public', 'movie.html'));
-  // stream.pipe(res);
 }
 
 module.exports = search;
